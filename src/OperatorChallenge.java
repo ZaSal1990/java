a2d5ee591b330557f95d258829077b42b7827e2c
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-10-20 06:51:39 -0600

    operator challenge complete

---
+++ src/OperatorChallenge.java
@@ -0,0 +1,19 @@
+public class OperatorChallenge {
+    public static void main (String[] args) {
+
+        double myDoubleOne = 20.0;
+        double myDoubleTwo = 80.0;
+        double result = (myDoubleOne + myDoubleTwo) * 100;
+        double myRemainder = result % 40.0;
+        boolean myBoolean = true ? true : false;
+        System.out.println(myBoolean);
+         if (!myBoolean) {
+             System.out.println("Got some remainder");
+         }
+
+
+
+
+    }
+
+}
