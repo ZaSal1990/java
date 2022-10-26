da24c771f2673a4629b10deafe8c387ac7361ffc
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-10-26 06:34:01 -0600

    read Int

--- src/userInput.java
+++ src/userInput.java
@@ -6,5 +6,9 @@ public class userInput {
         System.out.println("Enter your name: ");
         String name = scanner.nextLine();//prompts to read from next line
         System.out.println("Welcome " + name + "!");
+        System.out.println("Enter your year of birth: ");
+        int yearOfBirth = scanner.nextInt();//if use nextLine, parseString() will be required
+        System.out.println("You are " + (2022 - yearOfBirth) + " years old as of today!");
+        scanner.close(); //to free up resources
     }
 }
