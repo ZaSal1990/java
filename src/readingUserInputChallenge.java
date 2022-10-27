cd8d5ba31d40657e74daac9a6b7e5ee86f032330
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-10-27 07:09:57 -0600

    userImputChallenge

---
+++ src/readingUserInputChallenge.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class readingUserInputChallenge {
+
+    public static void main(String[] args) {
+    int counter = 1;
+    int sum = 0;
+        Scanner scanner = new Scanner(System.in);
+            while(counter <= 10 && 1) {
+                System.out.println("Enter number #" + counter + ":");
+                int number = scanner.nextInt();//prompts to read from next line
+                sum += number;
+                counter++;
+            }
+            System.out.println("The total is: " + sum);
+        scanner.close();
+    }
+}
