9c09fa67d5585c2760c6d99e6d52240a52c2a14c
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-11-01 21:29:44 -0600

    add

--- src/readingUserInputChallenge.java
+++ src/readingUserInputChallenge.java
@@ -8,9 +8,14 @@ public class readingUserInputChallenge {
         Scanner scanner = new Scanner(System.in);
             while(counter <= 10) {
                 System.out.println("Enter number #" + counter + ":");
-                int number = scanner.nextInt();//prompts to read from next line
-                sum += number;
-                counter++;
+               boolean isAnInt = scanner.hasNextInt();//prompts to read from next line
+                if (isAnInt) {
+                    int number = scanner.nextInt();
+                    sum += number;
+                    counter++;
+                } else {
+                    System.out.println("Invalid Number");
+                    }
             }
             System.out.println("The total is: " + sum);
         scanner.close();
