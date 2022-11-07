fb4bf6de6975b7fb808061bd146aab4a677665ce
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-11-07 13:17:13 -0700

    add getters

--- src/Account/Main.java
+++ src/Account/Main.java
@@ -11,6 +11,24 @@ public class Main {
         private double balance;
         private String customerName;
         private String email;
-    }
+        private double phoneNumber;
 
+
+        public double getAccountNumber() {
+            return accountNumber;
+        }
+        public double getBalance() {
+            return balance;
+        }
+        public String getCustomerName (){
+            return customerName;
+        }
+        public String getEmail () {
+            return email;
+
+        }
+        public double getPhoneNumber (){
+            return phoneNumber;
+        }
+    }
 }
