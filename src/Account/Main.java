c04e7d1e09b9422aa7b8df1e51dfc4c65f17ea54
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-11-08 12:17:03 -0700

    add withdraw and create user methods

--- src/Account/Main.java
+++ src/Account/Main.java
@@ -3,18 +3,28 @@ package Account;
 
 import java.util.Scanner;
 
+import static Account.Main.bankAccount.createUser;
+
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        bankAccount account = new bankAccount();
         System.out.println("Lets set you up as a user first");
+        bankAccount account = createUser(scanner); //calling instance creator and storing it as instance inside main
+        System.out.println("Welcome " + account.customerName);
         int actionNumber = 0;
         while (actionNumber != 9) {
             actionNumber = account.userInput(scanner); // to keep coming back to asking user input after each operation
             if (actionNumber == 1) {
                 System.out.println("Enter amount to deposit:");
                 System.out.println("Your current balance is: $" + account.depositFunds(scanner.nextInt()));
+            }
+            else if (actionNumber == 2) {
+                System.out.println("Enter amount to withdraw:");
+                System.out.println("Your current balance is: $" + account.withdrawFunds(scanner.nextInt()));
+            }
+            else if (actionNumber == 3) {
+                System.out.println("Your current balance is: $" + account.getBalance());
             }
         }
         scanner.close();
@@ -29,11 +39,21 @@ public class Main {
         private double phoneNumber;
 
         public int userInput(Scanner scanner) { //passing instance as argument
-            System.out.println("Enter: \n1: Deposit funds\n2: Check balance\n9: Exit");
+            System.out.println("Enter: \n1: Deposit funds\n2: Withdraw funds\n3: Check balance\n9: Exit");
             int userInput = scanner.nextInt();
             return userInput;
         }
-
+         public static bankAccount createUser(Scanner scanner) {
+             bankAccount account = new bankAccount();
+             System.out.println("Enter account holders name:");
+             account.setCustomerName(scanner.nextLine());
+             System.out.println("Enter email address:");
+             account.setEmail(scanner.nextLine());
+             System.out.println("Enter phone number:");
+             account.setEmail(scanner.nextLine());
+             System.out.println("Your current settings:\n" + "Username: " + account.getCustomerName() + "\nEmail: " + account.getEmail() + "\nPhone Number: " + account.getPhoneNumber());
+             return account;
+         }
         public double getAccountNumber() {
             return accountNumber;
         }
@@ -72,9 +92,11 @@ public class Main {
         }
         public double withdrawFunds (double decrement) {
             if (balance > decrement)
-            return balance -= decrement;
-            else System.out.println("Insufficient balance!");
-            return 0;
+                balance -= decrement;
+                else if (balance < decrement) {
+                    System.out.println("Cannot perform operation - Insufficient balance!");
+                }
+            return decrement;
         }
     }
 }
